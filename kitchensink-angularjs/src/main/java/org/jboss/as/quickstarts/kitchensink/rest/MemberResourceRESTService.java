ecd7880a15883995af684d4bae708a471d0ec6fe
Matt Drees <matt.drees@cru.org>
2013-06-27 12:28:58 -0400

    formatting - make consistent (slightly-tweaked default intellij formatting settings)

--- kitchensink-angularjs/src/main/java/org/jboss/as/quickstarts/kitchensink/rest/MemberResourceRESTService.java
+++ kitchensink-angularjs/src/main/java/org/jboss/as/quickstarts/kitchensink/rest/MemberResourceRESTService.java
@@ -16,12 +16,10 @@
  */
 package org.jboss.as.quickstarts.kitchensink.rest;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.logging.Logger;
+import com.google.common.collect.ImmutableMap;
+import org.jboss.as.quickstarts.kitchensink.data.MemberRepository;
+import org.jboss.as.quickstarts.kitchensink.model.Member;
+import org.jboss.as.quickstarts.kitchensink.service.MemberRegistration;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -40,11 +38,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import com.google.common.collect.ImmutableMap;
-import org.jboss.as.quickstarts.kitchensink.data.MemberRepository;
-import org.jboss.as.quickstarts.kitchensink.model.Member;
-import org.jboss.as.quickstarts.kitchensink.service.MemberRegistration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  * JAX-RS Example
@@ -54,7 +53,7 @@ import org.jboss.as.quickstarts.kitchensink.service.MemberRegistration;
 @Path("/members")
 @RequestScoped
 public class MemberResourceRESTService {
-    
+
     @Inject
     private Logger log;
 
@@ -105,13 +104,11 @@ public class MemberResourceRESTService {
 
         if (builder != null)
             return builder.build();
-        try
-        {
+        try {
             registration.register(member);
             // Create an "ok" response
             builder = Response.ok();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             builder = handleServerException(e);
         }
 
@@ -141,13 +138,11 @@ public class MemberResourceRESTService {
 
         if (builder != null)
             return builder.build();
-        try
-        {
+        try {
             registration.update(updatedMember);
             // Create an "ok" response
             builder = Response.status(Response.Status.NO_CONTENT);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             builder = handleServerException(e);
         }
 
@@ -160,8 +155,8 @@ public class MemberResourceRESTService {
                 Response
                     .status(Response.Status.NOT_FOUND)
                     .entity(ImmutableMap.of(
-                            "error", "there is no member whose id is " + id,
-                            "note", "this server only supports PUT for resource updates, not resource creation"))
+                        "error", "there is no member whose id is " + id,
+                        "note", "this server only supports PUT for resource updates, not resource creation"))
                     .build()
             );
     }
@@ -183,7 +178,7 @@ public class MemberResourceRESTService {
                     .status(Response.Status.BAD_REQUEST)
                     .entity(ImmutableMap.of("error", "id cannot be changed"))
                     .build()
-                );
+            );
     }
 
     private Response.ResponseBuilder validateMemberAndHandleExceptions(Member member) {
@@ -225,10 +220,10 @@ public class MemberResourceRESTService {
      * If the error is caused because an existing member with the same email is registered it throws a regular validation
      * exception so that it can be interpreted separately.
      * </p>
-     * 
+     *
      * @param member Member to be validated
      * @throws ConstraintViolationException If Bean Validation errors exist
-     * @throws ValidationException If member with the same email already exists
+     * @throws ValidationException          If member with the same email already exists
      */
     private void validateMember(Member member) throws ConstraintViolationException, ValidationException {
         // Create a bean validator and check for issues.
@@ -247,7 +242,7 @@ public class MemberResourceRESTService {
     /**
      * Creates a JAX-RS "Bad Request" response including a map of all violation fields, and their message. This can then be used
      * by clients to show violations.
-     * 
+     *
      * @param violations A set of violations that needs to be reported
      * @return JAX-RS response containing all violations
      */
@@ -269,10 +264,9 @@ public class MemberResourceRESTService {
     /**
      * Checks if a member with the same email address is already registered. This is the only way to easily capture the
      * "@UniqueConstraint(columnNames = "email")" constraint from the Member class.
-     * 
      *
      * @param newMemberId
-     * @param email The email to check
+     * @param email       The email to check
      * @return True if the email already exists, and false otherwise
      */
     public boolean emailAlreadyExists(Long newMemberId, String email) {
@@ -282,8 +276,7 @@ public class MemberResourceRESTService {
         } catch (NoResultException e) {
             // ignore
         }
-        if (member != null)
-        {
+        if (member != null) {
             return newMemberId == null || !newMemberId.equals(member.getId());
         }
         return false;
